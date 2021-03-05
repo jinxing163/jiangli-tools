f77d8a6e63d2d38b6d5d714377f66871a12cc392
jinxing <jinxing_jcdd@163.com>
2021-03-05 17:54:21 +0800

    commit kt

--- db-tools/src/main/java/com/jiangli/commons/client/tools/mybatis/MybatisGenerator.kt
+++ db-tools/src/main/java/com/jiangli/commons/client/tools/mybatis/MybatisGenerator.kt
@@ -26,26 +26,28 @@ fun main(args: Array<String>) {
 //    val TBL_NAME = "TBL_COMPANY_SHARE_STU"
 //    val TBL_NAME = "TBL_WHITE_LIST"
 //    val TBL_NAME = "TBL_COMMON_CATEGORY_ITEMS"
-    val TBL_NAME = "TBL_NOTICE"
+//    val TBL_NAME = "TBL_SPECIAL_TRAINING_CAMP"
+        val TBL_NAME = "WHITE_BOARD"
 //    val TBL_NAME = "TBL_MENU"
 //    val TBL_NAME = "TBL_USER"
 
 //    驼峰式 TBL_USER -> TblUser
     val JAVA_NAME = tblNameToCamel(TBL_NAME)
 //    或写死 discuss_id Change
-//    val JAVA_NAME = "TblLiveReplay"
+//    val JAVA_NAME = "ChatUserGroup"
 
     //     * 配置正确的aries-server项目后，生成的java、sql文件会自动拷贝至相应项目路径
     //     * 配置不正确也没关系，在本项目的target/sql_client_tools能找到生成文件，手动拷贝到项目路径即可
 //            val ARIES_SERVER_SRC_PATH = "E:\\idea_zhishi_workSpace\\aries-live-api-server"
 //        val ARIES_SERVER_SRC_PATH = "E:\\idea_zhishi_workSpace\\aries-server"
+//            val ARIES_SERVER_SRC_PATH = "E:\\idea_zhishi_workSpace\\aries-erp-server"
 //            val ARIES_SERVER_SRC_PATH = "E:\\idea_zhishi_workSpace_master\\aries-survey"
-            val ARIES_SERVER_SRC_PATH = "E:\\idea_zhishi_workSpace\\aries-base-message"
+//            val ARIES_SERVER_SRC_PATH = "E:\\idea_zhishi_workSpace\\aries-base-message"
 
 //          val ARIES_SERVER_SRC_PATH = "E:\\idea_zhishi_workSpace\\aries-base-video"
 //        val ARIES_SERVER_SRC_PATH = "E:\\idea_zhishi_workSpace\\aries-classtools"
 //        val ARIES_SERVER_SRC_PATH = "C:\\projects\\aries-erp-server"
-//    val ARIES_SERVER_SRC_PATH = "E:\\idea_zhishi_workSpace\\aries-live-server"
+    val ARIES_SERVER_SRC_PATH = "E:\\idea_zhishi_workSpace\\aries-live-server"
 //        val ARIES_SERVER_SRC_PATH = "C:\\projects\\aries-live-api-server"
 //        val ARIES_SERVER_SRC_PATH = "C:\\projects\\org-server"
     //////////////////////////////////////////////
